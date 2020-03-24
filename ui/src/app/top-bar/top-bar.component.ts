6eb37d32c626227fc27b88c9e6197dc37ac46db4
Masha <volkova.maria.d@gmail.com>
2020-03-25 02:27:57 +0300

    add dropdown bttn to menu

--- ui/src/app/top-bar/top-bar.component.ts
+++ ui/src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
-
-  constructor() { }
+  public isAuthenticated: boolean;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    //todo true - виден logout, profile, settings
+    //false - виден sign-in
+    this.isAuthenticated = true;
+  }
+
+  logOut() {
+    this.isAuthenticated = false;
+    return this.router.navigateByUrl('/home');
   }
 
 }

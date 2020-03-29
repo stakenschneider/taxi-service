335d6f9abea78b186464192ede4c4e29da4277e9
Masha <volkova.maria.d@gmail.com>
2020-03-30 00:05:03 +0300

    refactor css styles

--- ui/src/app/auth/forgot-password/forgot-password.component.ts
+++ ui/src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,18 +8,37 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
   public flag: boolean;
+  public emailErrorFlag: boolean;
   public email = '';
   @ViewChild('emailInput') emailInput: ElementRef;
 
-  constructor() {
-    this.flag = false;
+
+  constructor(private router: Router) {
+    this.flag = true;
   }
 
   ngOnInit(): void {
   }
 
   sendMail() {
+    this.flag = this.emailInput ? this.flag : false;
+
     this.email = this.emailInput.nativeElement.value;
-    this.flag = true;
+    if (this.isEmail(this.email)) {
+      this.flag = false;
+    } else {
+      this.emailErrorFlag = true;
+    }
+  }
+
+
+  signIn() {
+    return this.router.navigateByUrl('/sign-in');
+  }
+
+  // TODO sign-up component
+  isEmail(search: string): boolean {
+    // tslint:disable-next-line:max-line-length
+    return new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(search);
   }
 }

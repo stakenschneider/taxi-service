4a21e533ae5db77ddd6447453994853dddb58c18
Masha <volkova.maria.d@gmail.com>
2020-03-30 00:47:44 +0300

    ui to SPA

--- ui/src/app/top-bar/top-bar.component.ts
+++ ui/src/app/top-bar/top-bar.component.ts
@@ -1,21 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {StoreService} from '../../services/store.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent implements OnInit, OnDestroy {
   public isAuthenticated: boolean;
-  constructor(private router: Router) { }
+  private authChangedSubscriber: Subscription;
+  constructor(private router: Router, private storeService: StoreService) {
+  }
 
   ngOnInit(): void {
+    this.authChangedSubscriber = this.storeService.authChanged.asObservable().subscribe(x => {
+      this.isAuthenticated = x;
+    });
     // TODO true - виден logout, profile, settings | false - виден sign-in привязка к StoreService
   }
 
+  ngOnDestroy(): void {
+    this.authChangedSubscriber.unsubscribe();
+  }
+
   logOut() {
-    this.isAuthenticated = false;
+    this.storeService.setIsAuth(false);
     return this.router.navigateByUrl('/home');
+  }
+
+  home() {
+    return this.router.navigateByUrl('/home');
+  }
+
+  notFound() {
+    return this.router.navigateByUrl('/not-found');
+  }
+
+  profile() {
+    return this.router.navigateByUrl('/profile');
+  }
+
+  requestCar() {
+    return this.router.navigateByUrl('/request-care');
+  }
+
+  settings() {
+    return this.router.navigateByUrl('/settings');
   }
 }

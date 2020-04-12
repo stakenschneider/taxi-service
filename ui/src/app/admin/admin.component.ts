8ba5673c40624f3e4c4340b2928b64b99337f705
Masha <volkova.maria.d@gmail.com>
2020-04-12 22:27:30 +0300

    added person table

--- ui/src/app/admin/admin.component.ts
+++ ui/src/app/admin/admin.component.ts
@@ -1,14 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {Client} from '../../models/actor/client.model';
-import {Driver} from '../../models/actor/driver.model';
-import {Router} from '@angular/router';
-import {DriverService} from '../../services/driver.service';
-import {AdminService} from '../../services/admin.service';
-import {DataService} from '../../services/data.service';
-import {StoreService} from '../../services/store.service';
-import {MatTableDataSource} from '@angular/material/table';
-import {Trip} from '../../models/trip.model';
-import {MatSort} from '@angular/material/sort';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -16,31 +6,9 @@ import {MatSort} from '@angular/material/sort';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  public clientsArray: Client[];
-  public driversArray: Driver[];
 
-  tripDataSource: MatTableDataSource<Trip>;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-
-  constructor(private router: Router, private driverService: DriverService, private adminService: AdminService,
-              private dataService: DataService, private storeService: StoreService) {
+  constructor() {
   }
 
   ngOnInit(): void {}
-
-  showAllClients() {
-    this.adminService.getAllDrivers().subscribe(
-      data => {
-        this.clientsArray = data.body;
-      }, error => alert(error)
-    );
-  }
-
-  showAllDrivers() {
-    this.adminService.getAllDrivers().subscribe(
-      data => {
-        this.driversArray = data.body;
-      }, error => alert(error)
-    );
-  }
 }

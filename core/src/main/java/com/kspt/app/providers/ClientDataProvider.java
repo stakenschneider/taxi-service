2c0f0ceb711065a48f3b6677699e0692bc5e3751
Masha <volkova.maria.d@gmail.com>
2020-04-21 22:56:37 +0300

    and client #4

--- core/src/main/java/com/kspt/app/providers/ClientDataProvider.java
+++ core/src/main/java/com/kspt/app/providers/ClientDataProvider.java
@@ -1,10 +1,15 @@
 package com.kspt.app.providers;
 
+import com.kspt.app.entities.actor.Client;
 import com.kspt.app.models.response.ResponseOrMessage;
 import com.kspt.app.models.table.GridDataModel;
+import com.kspt.app.models.table.MetaDataModel;
+import com.kspt.app.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,8 +18,41 @@ import java.util.Map;
 @Component
 @Qualifier("clientTable")
 public class ClientDataProvider implements IDataProvider {
+    ClientRepository clientRepository;
+
+    public ClientDataProvider(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
     @Override
     public ResponseOrMessage<GridDataModel> getData(Map<String, Object> parameters) {
-        return null;
+        List<Client> clients = clientRepository.findAll();
+        if (clients.isEmpty()) {
+            return new ResponseOrMessage<>("Clients not found");
+        }
+        GridDataModel dataModel = new GridDataModel();
+        MetaDataModel metaDataModel = new MetaDataModel();
+
+        String[] columns = {"No.", "First Name", "Last Name", "Email","Login", "Phone Number", "Rating","Deleted"};
+        metaDataModel.setColumns(columns);
+        int countOfClients = clients.size();
+        metaDataModel.setTotalCount(countOfClients);
+
+        ArrayList<ArrayList<Object>> data = new ArrayList<>();
+        clients.forEach(client -> {
+            ArrayList<Object> row = new ArrayList<>();
+            row.add(client.getId());
+            row.add(client.getFirstName());
+            row.add(client.getLastName());
+            row.add(client.getCredentials().getEmail());
+            row.add(client.getCredentials().getUsername());
+            row.add(client.getPhoneNumber());
+            row.add(client.getRating());
+            row.add(client.isDeleted());
+            data.add(row);
+        });
+        dataModel.setData(data);
+        dataModel.setMetaData(metaDataModel);
+        return new ResponseOrMessage<>(dataModel);
     }
 }
